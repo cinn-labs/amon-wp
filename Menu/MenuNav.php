3e5fd87c2810a736efa2e3e6f19fb8e45b951458
Christian Storch <ccstorch@gmail.com>
2016-08-27 14:13:52 +0100

    MenuNav component

---
+++ Menu/MenuNav.php
@@ -0,0 +1,21 @@
+<?php get_template_part( 'node_modules/amon-wp/_helpers/propsFormatter' ); ?>
+
+<?php
+  if( !function_exists('MenuNav_BEGIN') ) : function MenuNav_BEGIN($props = array()) {
+
+    $props['class'] = array_key_exists('class', $props) ? $props['class'] : '';
+    $props['class'] .= ' cnMenuNav';
+    $props['wrapper'] = 'nav';
+
+    Content_BEGIN($props);
+
+  } endif;
+
+  if( !function_exists('MenuNav_END') ) : function MenuNav_END($props = array()) {
+
+    $props['wrapper'] = 'nav';
+
+    Anchor_END($props);
+
+  } endif;
+?>
