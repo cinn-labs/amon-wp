6eb2ff464e2210858432a67a8e539f8424a44f9f
Christian Storch <ccstorch@gmail.com>
2016-11-23 17:05:57 +0000

    Image component with option to pass the image id

--- Image/Image.php
+++ Image/Image.php
@@ -10,14 +10,17 @@
     // Src
     $imageName = array_key_exists('fileName', $props) ? $props['fileName'] : false;
     $filePath = array_key_exists('filePath', $props) ? $props['filePath'] : false;
+    $imageId = array_key_exists('fileId', $props) ? $props['fileId'] : false;
+    $imageSize = array_key_exists('size', $props) ? $props['size'] : 'large';
     if($filePath) {
       $attrs .= 'src="' . $filePath . '" ';
     } else if($imageName) {
       $attrs .= 'src="' . getAsset($imageName) . '" ';
+    } else if($imageId) {
+      $attrs .= 'src="' . wp_get_attachment_image_src($imageId, $imageSize)[0] . '" ';
     } else {
       $field = array_key_exists('field', $props) ? $props['field'] : '';
       $postId = array_key_exists('postId', $props) ? $props['postId'] : NULL;
-      $imageSize = array_key_exists('size', $props) ? $props['size'] : 'large';
       $image = get_field($field, $postId);
       $attrs .= 'src="' . $image['sizes'][$imageSize] . '" ';
     }
